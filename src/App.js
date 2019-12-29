dbbbe3a34f26a097b8af1eacb182230c3e42ce76
laiba18 <laiba2618@gmail.com>
2019-12-29 15:52:56 +0500

    todoapp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor(){
+    super();
+  }
+  state={
+    todos:[],
+    ctodo : ''
+  }
+
+
+   addtodo = () => {
+    let c= this.state.todos.slice();
+    c.push(this.state.ctodo);
+    this.setState({
+         todos : c,
+         ctodo :''
+    });
+  }
+  
+  handleChange = (e) => {
+          this.setState({ctodo:e.target.value});
+ }
+
+  deletetodo = (i) =>{
+    let d= this.state.todos.slice();
+    d.splice(i,1);
+    this.setState({
+            todos: d,
+            ctodo : ''
+    });
+  }
+ 
+  render(){
+    let b=this.state.todos.map((e,i) =>
+    {
+      return(
+        <li key={i} ><h1>{e} <span onClick={() => {this.deletetodo(i)}}>x</span></h1></li>
+      );
+    });
+
+    return(
+      <div className="container" id="cont1">
+             <center><h1>TODO APPLICATION </h1></center><br></br>
+                    <div className="row">
+                    <div className="col-md-10">
+        <input type="text"  value={this.state.ctodo}  onChange={this.handleChange}  placeholder = "Enter a new todo"  className="form-control"/>      
+                    </div>
+                    <div className="col-md-2">
+                    <button className="btn-btn-light" onClick={this.addtodo}>ADD TODO</button>
+                    </div>
+                    </div>
+                    <br></br>
+                    {this.state.todos.length  === 0 ? <h1> NO TODOS YET !!!!</h1> : <ul>{b}</ul>}
+                    <br></br>
+      </div>
+    );
+  }
 }
+
 
 export default App;
